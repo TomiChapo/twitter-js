0f016993d99d9bb2e78d0d8407bc3b4fd2ecb2ac
dieferrari <die.a.ferrari@gmail.com>
2018-01-24 11:17:07 -0300

    iniciado

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require( 'express' );
+const app = express();
+
+app.use('/', function (req, res, next) {
+	res.send('Haz llegado a tu destino viajero');
+	next();    
+})
+
+app.use('/special',function (req, res, next){
+	res.send('Has llegado a un destino especial');
+	next();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+listen(3005)
